d3f5ffb4ef657de28c947abc13f46adf251b8c3f
Matthew Bischoff <bischoff2014@gmail.com>
2017-01-18 22:03:13 +0000

    Added code comments

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,26 +1,39 @@
 class ContactsController < ApplicationController
-   
- def new
+  
+  # GET request to /contactu-us
+  # Show new contact form
+  def new
    @contact = Contact.new
- end
- 
- def create
-   @contact = Contact.new(contact_params)
-   if @contact.save
-     name = params[:contact][:name]
-     email = params[:contact][:email]
-     body = params[:contact][:comments]
-     ContactMailer.contact_email(name, email, body).deliver
-     flash[:success] = "Message sent."
-     redirect_to new_contact_path #, notice: "Message sent."
-   else
-     flash[:danger] = @contact.errors.full_messages.join(", ")
-     redirect_to new_contact_path #, notice: "Error occured."
-   end
- end
- 
- private
-  def contact_params
-    params.require(:contact).permit(:name, :email, :comments)
   end
+  
+  # POST request /contacts
+  def create
+    # Mass assigment of form fields into Contact object
+    @contact = Contact.new(contact_params)
+    # Save the Contact object to the database
+    if @contact.save
+      # Store form fileds via parameters, into variables
+      name = params[:contact][:name]
+      email = params[:contact][:email]
+      body = params[:contact][:comments]
+      # Plug vairables into Contact Mailer
+      # email method and send email
+      ContactMailer.contact_email(name, email, body).deliver
+      # If Contact object doesn't save,
+      # store errors in flash has,
+      # and redirect to the new action
+      flash[:success] = "Message sent."
+      redirect_to new_contact_path #, notice: "Message sent."
+    else
+      flash[:danger] = @contact.errors.full_messages.join(", ")
+      redirect_to new_contact_path #, notice: "Error occured."
+    end
+  end
+  
+  private
+  # To collect data from form, we need to use
+  # strong parameters and whitelist the form fields
+    def contact_params
+      params.require(:contact).permit(:name, :email, :comments)
+    end
 end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,5 @@
 class Contact < ActiveRecord::Base
+  # Conatct form validations
   validates :name, presence: true
   validates :email, presence: true
   validates :comments, presence: true
